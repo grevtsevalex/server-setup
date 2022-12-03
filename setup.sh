5f96d74fc6ac3570b4e83ccabd2520c7e5ba533a
grevtsevalex <grevtsevalex@dns-shop.ru>
2022-12-03 21:15:07 +1000

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir /var/www -p
 sudo chgrp -R www-data /var
@@ -6,18 +6,16 @@ sudo usermod -a -G www-data $USER
 sudo chmod -R 775 /var
 sudo su $(whoami)
 
-#install ansible
 sudo apt update -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
 python3 -m pip install --user ansible
 sudo apt install ansible -y
 
-#install make
 apt install make -y
 
 
 #generate ssh
-echo "Сгенерируй ssh-ключ командой: ssh-keygen"
-echo "Сгенерированный публичный ssh-ключ нужно добавить в gitlab";
-echo "Выполни команду: ssh-keyscan git.dns-shop.ru >> ~/.ssh/known_hosts"
+echo "!!!!!  Сгенерируй ssh-ключ командой: ssh-keygen    !!!!!"
+echo "!!!!!  Сгенерированный публичный ssh-ключ нужно добавить в gitlab    !!!!!";
+echo "!!!!!  Выполни команду: ssh-keyscan git.dns-shop.ru >> ~/.ssh/known_hosts !!!!!"
